72f23c10895f65744e831ff501619e4679d9f487
moufatmi <mou.fatmi@edu.umi.ac.ma>
2026-02-18 03:37:11 +0000

    Fix base path and CNAME for taskin.beausejourvoyage.com

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'taskin.beausejourvoyage.com',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
